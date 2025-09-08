d3e3d253d31025d9f77dca22eb776d5a206ec630
addem <kwesi.eddem@gmail.com>
2025-09-08 18:00:58 +0000

    completed the basic operations file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_of_numbers = number1 + number2
+difference_of_numbers = number1 - number2
+product_of_numbers = number1 * number2
+
+print('Addition of '+str(number1)+' and '+str(number2)+' is '+str(sum_of_numbers))
+print('Subtraction of '+str(number1)+' and '+str(number2)+' is '+str(difference_of_numbers))
+print('AdMultiplication of '+str(number1)+' and '+str(number2)+' is '+str(product_of_numbers))
