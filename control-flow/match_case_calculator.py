c1fa6c5e1190956135adb940d401280ed15e837c
addem <kwesi.eddem@gmail.com>
2025-09-16 13:00:46 +0000

    simple calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+first = float(input('Enter the first number: '))
+second = float(input('Enter the second number: '))
+operator = input('Choose the operator (+, -, *, /): ')
+
+match operator:
+    case '+':
+        print(f'The result is {first + second}')
+    case '-':
+        print(f'The result is {first - second}')
+    case '*':
+        print(f'The result is {first * second}')
+    case '/':
+        if second == 0:
+            print ('Cannot divide by zero')
+        else:
+            print(f'The result is {first / second}')
+    case _:
+        print(f'Invalid entry.')
