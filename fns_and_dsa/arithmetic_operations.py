dbc0012496f459420a988b896d0ae6f9b3acd547
addem <kwesi.eddem@gmail.com>
2025-09-26 11:27:18 +0000

    3rd modification

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,9 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             return num1 * num2
         case 'divide':
-            if num2 == 0:
-                return "Division by 0 is not allowed."
-            else:
+            if num2 > 0:
                 return num1 / num2
+            elif num2 == 0:
+                return "Division by 0 is not allowed."
+        case _:
+            return "Invalid operand."
