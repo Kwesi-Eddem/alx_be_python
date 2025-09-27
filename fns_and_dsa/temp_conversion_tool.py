e551330b5cdcfdd19432923a037e3d2607e75f71
addem <kwesi.eddem@gmail.com>
2025-09-27 11:45:37 +0000

    1st commit for temp tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,21 @@
+FARENHEIT_TO_CELCIUS_FACTOR = 5/9
+CELCIUS_TO_FARENHEIT_FACTOR = 9/5
+
+def convert_to_celcius(farenheit):
+    temp_in_celcius = (farenheit-32) * FARENHEIT_TO_CELCIUS_FACTOR
+    return temp_in_celcius
+
+def convert_to_farenheit(celcius):
+    temp_in_farenheit = (celcius * CELCIUS_TO_FARENHEIT_FACTOR) + 32
+    return temp_in_farenheit
+
+inputValue = float(input("Enter the temperature to convert: "))
+temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+match temp_type.upper():
+    case 'F':
+        print(f"{inputValue}°F is {convert_to_celcius(inputValue)}°C")
+    case 'C':
+        print(f"{inputValue}°C is {convert_to_farenheit(inputValue)}°F")
+    case _:
+        print()
