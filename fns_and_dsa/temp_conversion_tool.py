fcc61d08cd10a291257fb38682c656e5e5460e01
addem <kwesi.eddem@gmail.com>
2025-09-27 11:55:06 +0000

    3rd commit for temp tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,21 @@
-FARENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FARENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celcius(farenheit):
-    temp_in_celcius = (farenheit-32) * FARENHEIT_TO_CELSIUS_FACTOR
-    return temp_in_celcius
+def convert_to_celsius(fahrenheit):
+    temp_in_celsius = (fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return temp_in_celsius
 
-def convert_to_farenheit(celcius):
-    temp_in_farenheit = (celcius * CELSIUS_TO_FARENHEIT_FACTOR) + 32
-    return temp_in_farenheit
+def convert_to_fahrenheit(celsius):
+    temp_in_fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return temp_in_fahrenheit
 
 inputValue = float(input("Enter the temperature to convert: "))
 temp_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
 match temp_type.upper():
     case 'F':
-        print(f"{inputValue}°F is {convert_to_celcius(inputValue)}°C")
+        print(f"{inputValue}°F is {convert_to_celsius(inputValue)}°C")
     case 'C':
-        print(f"{inputValue}°C is {convert_to_farenheit(inputValue)}°F")
+        print(f"{inputValue}°C is {convert_to_fahrenheit(inputValue)}°F")
     case _:
         print()
