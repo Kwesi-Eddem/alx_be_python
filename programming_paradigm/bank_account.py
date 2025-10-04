9b5bccd53a996d21ab4ac6496425bcffc96eb20b
Addem <kwesi.eddem@gmail.com>
2025-10-04 14:10:34 +0000

    bank_account.py created

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount():
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        account_balance = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print('Amount must be greater than 0.')
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance}")
