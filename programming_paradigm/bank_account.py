5408e5b3f72dc17109bfc384a625b768d4f9b7fc
Addem <kwesi.eddem@gmail.com>
2025-10-04 14:34:47 +0000

    bank_account.py 7th edit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,20 +5,17 @@ class BankAccount():
         self.account_balance = account_balance
 
     def deposit(self, amount):
-        print("BankAccount:")
         if amount > 0:
             self.account_balance += amount
         else:
             print('Amount must be greater than 0.')
 
     def withdraw(self, amount):
-        print("BankAccount:")
         if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
             return False
 
-    def display_balance(self):
-        
+    def display_balance(self):  
         print(f"Current Balance: ${self.account_balance:.2f}")
