2217bd62533a0515427be53425d46bb1dece7afa
Addem <kwesi.eddem@gmail.com>
2025-10-04 14:21:07 +0000

    bank_account.py 3rd edit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,4 +18,4 @@ class BankAccount():
 
     def display_balance(self):
         print("BankAccount:")
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
