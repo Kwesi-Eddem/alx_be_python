80667911b5c7f621ff8fa30df916c2a5dc2a69cf
addem <kwesi.eddem@gmail.com>
2025-10-13 15:04:29 +0000

    1st commit for csmd.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,1 +1,11 @@
-print('class_static_methods_demo.py')
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add (a,b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a,b):
+        print(f"Calculaton type: {cls.calculation_type}")
+        return a * b
